8d179b71205d95c17856d8dd8f72f511305abb28
denis.osmanovic <denis.osmanovic@virginpulse.com>
2020-10-09 10:49:54 +0200

    Initial Commit

---
+++ Movie App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie App
+//
+//  Created by Denis Osmanovic on 10/9/20.
+//  Copyright © 2020 Denis Osmanovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
